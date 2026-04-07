33324b8d65401d8b95fbba0440255b8c54e506b4
abdulbasitfaruq <abdulbasitfaruq26@gmail.com>
2026-04-07 16:16:23 +0100

    add loginPage and routhing setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import HomePage from './pages/HomePage'
+import {Routes, Route} from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
 
 function App() {
     return (
         <div>
             <Navbar />
-            <HomePage />
+
+            <main className="min-h-screen">
+                <Routes>
+                    <Route path="/" element={<HomePage  />} />
+                    <Route path="/login" element={<LoginPage />} />
+                </Routes>
+            </main>
+
             <Footer />
         </div>
     )
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate,Link } from 'react-router-dom';
-import { loginUser } from '../services/authService';
+import { loginUser } from '../services/api';
 
 function LoginPage() {
     const navigate = useNavigate();
